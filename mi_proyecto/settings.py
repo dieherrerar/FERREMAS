9ab1b002425bf343ba33e785de27dd7350dd2452
MkOMIN <minibastivega93@gmail.com>
2025-07-01 21:33:01 -0400

    a

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -37,8 +37,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ] 
 ROOT_URLCONF = 'mi_proyecto.urls'
 
